e7dc36bb1ae9a12afed32810dfa153b9ac1810a7
Sarthak <sarthakanand1409@gmail.com>
2026-06-13 01:24:08 +0530

    Project Structure Ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sarthak Anand",
+    author_email="sarthakanand1409@gmail.com",
+    packages=find_packages()
+)
